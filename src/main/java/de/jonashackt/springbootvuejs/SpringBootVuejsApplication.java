e3541c4d052dd4d591ae23729ffa313fc39969aa
Jonas Hecht <jonas.hecht@codecentric.de>
2021-05-12 07:35:08 +0200

    Enable CORS headers for other domain sources (act as API service). Therefore also removed the SpaRedirectFilterConfiguration.java

--- src/main/java/de/jonashackt/springbootvuejs/SpringBootVuejsApplication.java
+++ src/main/java/de/jonashackt/springbootvuejs/SpringBootVuejsApplication.java
@@ -2,6 +2,9 @@ package de.jonashackt.springbootvuejs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringBootVuejsApplication {
@@ -9,4 +12,16 @@ public class SpringBootVuejsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootVuejsApplication.class, args);
 	}
+
+	// Enable CORS globally
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/*").allowedOrigins("*");
+			}
+		};
+	}
+
 }
